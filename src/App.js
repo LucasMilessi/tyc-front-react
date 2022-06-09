e976830dc8aba0560d56a92f929fb01f4c95caba
Lucas <lucasmilessi98@gmail.com>
2022-06-09 17:05:18 -0300

    Terminado

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
 import { useEffect, useState } from 'react';
-import Popup from 'reactjs-popup';
-import 'reactjs-popup/dist/index.css';
-
+import { Modal, Button } from 'react-bootstrap';
+import { Formik, Field, Form } from "formik";
+import * as Yup from 'yup';
 
 const URL_API = 'http://localhost:8082/api/termYcond'
 
+const docSchema = Yup.object().shape({
+  documento: Yup.string()
+    .required("Campo obligatorio"),
+  checkB: Yup.boolean()
+});
+
 function App() {
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [show, setShow] = useState(false);
   const [tyc, setTyc] = useState([]);
   const [tipoDoc, setTipoDoc] = useState();
-  const [doc, setDoc] = useState();
   const [check, setCheck] = useState(false);
   const [estado, setEstado] = useState(true);
+  const [boton, setBoton] = useState(true);
 
-  const AbrirPagina = () => {
-    window.open("Hola")
-  }
+
+  const validarCedula = (value) => {
+    const cedulaValida = /[0-9]{2}-PN-[0-9]{3}-[0-9]{4}/;
+    
+    if(cedulaValida.test(value.replace(/\s/g, ""))){
+      setBoton(false)
+    }else{
+      setBoton(true)
+    }
+
+    return cedulaValida.test(value.replace(/\s/g, ""))
+      ? undefined
+      : 'La cédula no es correcta, ej: 12-PN-123-1234';
+  };
+
+  const validarPasaporte = (value) => {
+    const pasaporteValido = /^[a-zA-Z0-9-]{5,16}$/;
+    
+    if(pasaporteValido.test(value.replace(/\s/g, ""))){
+      setBoton(false)
+    }else{
+      setBoton(true)
+    }
+
+    return pasaporteValido.test(value.replace(/\s/g, ""))
+      ? undefined 
+      : 'El pasaporte no es correcto, debe de contener de 5 a 16 caracteres, ej: 123-PN-12';
+      
+  };
+
 
   useEffect(() => {
     fetch(URL_API + '/mostrarTyC')
@@ -29,7 +66,7 @@ function App() {
 
     let datos = {
       tipoDocumento: tipoDoc,
-      documento: doc,
+      documento: event.documento,
       versionTC: tyc.version
     };
 
@@ -56,14 +93,16 @@ function App() {
         console.log("Error: ", error);
       });
 
+    setCheck(check);
+    setEstado(true);
+    setBoton(true);
 
-    setCheck(!check);
-    setEstado(false);
   }
 
   const HabilitarCheck = () => {
     setCheck(!check);
     setEstado(false);
+    handleClose();
   }
 
   return (
@@ -76,9 +115,11 @@ function App() {
       <br />
       <br />
 
+
       <p className='m-4 row justify-content-center'>Tipo de Documento</p>
 
-      <div class="container input-group">
+
+      <div className="container input-group">
         <select className='form-select form-select-lg mb-3' onChange={event => { setTipoDoc(event.target.value) }}>
           <option className='btn btn-primary'>Elegir Tipo documento</option>
           <option value="Cedula">Cedula</option>
@@ -90,36 +131,70 @@ function App() {
       <br />
       <br />
 
-      <div class="container input-group">
-        <span class="input-group-text" id="inputGroup-sizing-lg">Número de Documento</span>
-        <input type="text"
-          class="form-control"
-          placeholder='Ingrese su numero de documento'
-          aria-label="Large"
-          aria-describedby="inputGroup-sizing-sm"
-          onChange={event => { setDoc(event.target.value) }} />
-      </div>
+      <Formik
+        initialValues={{
+          documento: '',
+          checkB: false
+        }}
+        validationSchema={docSchema}
+        onSubmit={(values) => AgregarAceptTyC(values)}
+      >
+        {({ errors, touched }) => (
+          <Form>
 
-      <br />
-      <br />
-      <br />
-
-      <div class="container">
-        <div class="form-check">
-          <input class="form-check-input m-3 row justify-content-center"
-            type="checkbox"
-            value="{check}"
-            disabled={estado}
-            onChange={AgregarAceptTyC}
-          />
-          <Popup trigger={<button className='btn btn-warning m-2'>Leer Terminos y Condiciones</button>} position="right center">
-            <div>
-              {tyc.descripcion}
-              <button className='btn btn-warning m-3' onChange={HabilitarCheck}>Aceptar TyC</button>
+            <div className="container input-group">
+              <span className="input-group-text" id="inputGroup-sizing-lg">Número de Documento</span>
+              <Field
+                id="documento"
+                type="text"
+                className="form-control"
+                name="documento"
+                placeholder='Ingrese su numero de documento'
+                aria-label="Large"
+                aria-describedby="inputGroup-sizing-sm" 
+                validate={tipoDoc === 'Pasaporte' ? validarPasaporte : validarCedula}
+                 />
             </div>
-          </Popup>
-        </div>
-      </div>
+
+            {errors.documento && touched.documento ? (
+              <div className='m-4 row justify-content-center'>{errors.documento}</div>
+            ) : null}
+
+            <br />
+            <br />
+            <br />
+
+            <div className="container">
+              <div className="form-check">
+                <Field className="form-check-input m-4 row justify-content-center"
+                  type="checkbox"
+                  name="checkB"
+                  value="{check}"
+                  disabled={estado}
+                  onChange={AgregarAceptTyC}
+                />
+              </div>
+
+              <Button className="nextButton" onClick={handleShow}>
+                Ver Terminos y Condicones
+              </Button>
+
+            </div>
+
+            <Modal show={show} onHide={handleClose} backdrop='static' keyboard="false">
+              <Modal.Header closeButton>
+                <Modal.Title>Terminos y Condiciones</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>{tyc.descripcion}</Modal.Body>
+              <Modal.Footer>
+                <Button variant="primary" onClick={HabilitarCheck} type="submit" disabled={boton} onChange={handleClose}>
+                  Aceptar TyC
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 }
